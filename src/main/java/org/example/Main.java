59a7c66102398ec00e3a630ad24e52450289496e
n3xusw1sp <n3xusw1sp@gmail.com>
2024-07-06 20:55:35 +0300

    1st lesson

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+class Main {
+
+    public static void main(String[] args) {
+        // В один рядок:
+        // System.out.print("Hello World!");
+
+        // В різні рядки:
+        /* System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("!"); */
+
+        // Слово в інший рядок:
+        // System.out.print("Hello \n\n\nWorld!");
+
+        // Слово в інший рядок + табуляція:
+        // System.out.print("Hello \n\tWorld!");
+    }
+
+}
