2e31475e7298b332de40e29b6b119725a3b026e1
n3xusw1sp <n3xusw1sp@gmail.com>
2024-07-07 18:06:55 +0300

    july7

---
+++ src/main/java/org/example/Lesson_1.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Lesson_1 {
+    public static void main(String[] args) {
+        // ВИВЕДЕННЯ ІНФОРМАЦІЇ:
+
+
+        // В один рядок:
+        System.out.print("Hello World!");
+
+        // В різні рядки:
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("!");
+
+        // Слово в інший рядок:
+        System.out.print("Hello \n\n\nWorld!");
+
+        // Слово в інший рядок + табуляція:
+        System.out.print("Hello \n\tWorld!");
+
+        // Цифри:
+        System.out.print(500);
+    }
+}
---
+++ src/main/java/org/example/Lesson_3.java
@@ -0,0 +1,58 @@
+package org.example;
+import java.util.Scanner;
+
+public class Lesson_3 {
+    public static void main (String[] args) {
+        // Дані від користувача
+        // https://www.youtube.com/watch?v=9cV6MSE2IMg
+
+
+        /*
+           scan.nextFloat / Double - зчитує числа з .
+           scan.nextLine - зчитує слова
+           scan.nextInt - зчитує цілі числа
+        */
+
+        /*
+           Scanner scan = new Scanner(System.in); // ініціалізація сканера
+
+           String text = scan.nextLine();
+           int userNum = scan.nextInt();
+           float userFloat = scan.nextFloat();
+
+
+           System.out.println(text);
+           System.out.println(userNum);
+           System.out.println(userFloat);
+        */
+
+        // Математичні дії
+
+        // Замість + може бути: -, *, /, %
+        // Замість += може бути -=, *=, /=, %=
+
+        /*
+        int a = 5, b = 10, res;
+        res = a + b;
+        res += 5; // Тут ми додали до змінної res число 5
+        res++; // Тут ми додали до змінної res 1
+        System.out.println(res);
+        */
+
+        // КАЛЬКУЛЯТОР:
+
+
+        /*
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Enter 1 num: ");
+        int num1 = scan.nextInt();
+
+        System.out.println("Enter 2 num: ");
+        int num2 = scan.nextInt();
+
+        System.out.println("Result: " + (num1 + num2));
+         */
+    }
+}
